25bb7dfb1cf56da520e10294085800f87bf166b0
tyxchen <tyxchen@outlook.com>
2017-10-07 11:19:07 -0400

    fixes #796; C comments at EOF are now recognized

--- lib/rouge/lexers/c.rb
+++ lib/rouge/lexers/c.rb
@@ -90,7 +90,7 @@ module Rouge
 
       state :whitespace do
         rule /\n+/m, Text, :bol
-        rule %r(//(\\.|.)*?\n), Comment::Single, :bol
+        rule %r(//(\\.|.)*?$), Comment::Single, :bol
         mixin :inline_whitespace
       end
 
